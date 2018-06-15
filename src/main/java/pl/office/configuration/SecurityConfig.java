c82b27a30fba78a12305306d999d06e4adebe9ad
szymon <mantajszymon@gmail.com>
2018-06-15 21:43:03 +0200

    importy

--- src/main/java/pl/office/configuration/SecurityConfig.java
+++ src/main/java/pl/office/configuration/SecurityConfig.java
@@ -1,8 +1,5 @@
 package pl.office.configuration;
 
-import java.util.Arrays;
-import java.util.List;
-
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
